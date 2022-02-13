e3720d282220769e1a9bb842e65d1eeeee1cc026
zwh <270602532@qq.com>
2022-02-13 14:52:10 +0800

    更新run.py

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from config import conf
 import os
 
 if __name__ == '__main__':
-    #12334
+    #123345
     report_path = conf.get_report_path() + os.sep + "result"
     report_html_path = conf.get_report_path() + os.sep + "html"
     pytest.main(["-s","--alluredir", report_path])
